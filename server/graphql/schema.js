f514e63767bc788e8fb7de58a1842bed46a45a50
mcintdre000 <38018234+mcintdre000@users.noreply.github.com>
2018-06-27 15:09:42 -0700

    Finished mini

--- server/graphql/schema.js
+++ server/graphql/schema.js
@@ -1,4 +1,4 @@
-const characters = require('./model')
+let characters = require('./model')
 const axios = require('axios')
 const {
     GraphQLSchema,
@@ -15,6 +15,7 @@ const MovieType = new GraphQLObjectType({
     name: 'Movie',
     fields: () => {
         return {
+            episode_id: { type: GraphQLInt },
             title: { type: GraphQLString },
             releaseDate: { 
                 type: GraphQLString,
@@ -25,6 +26,30 @@ const MovieType = new GraphQLObjectType({
         }
     }
 })
+
+// const MovieType2 = new GraphQLObjectType({
+//     name: 'Movie Planet',
+//     fields: () => {
+//         return {
+//             name: { type: GraphQLString },
+//             resolve: person => {
+//                 return person.films.episode_id
+//             }
+//             }
+//         }
+//     })
+
+const HomeWorldType = new GraphQLObjectType({
+    name: 'HomeWorld',
+    fields: () => {
+      return {
+        name: { type: GraphQLString },
+        climate: { type: GraphQLString },
+        population: { type: GraphQLString }
+      }
+    }
+  })
+
 
 const PersonType = new GraphQLObjectType({
     name: 'Person',
@@ -40,8 +65,13 @@ const PersonType = new GraphQLObjectType({
                         return axios.get(film).then(res => res.data)
                     })
                 }
-            }
-            
+
+            },
+            homeWorld: {
+              type: HomeWorldType,
+              resolve: (person) => {
+                return axios.get(person.homeworld).then(res => res.data)
+              }
             }
         }
     }
@@ -55,11 +85,58 @@ const Query = new GraphQLObjectType({
             people: {
                 type: new GraphQLList(PersonType),
                 resolve: () => characters
+            },
+            person: {
+                type: PersonType,
+                args: {
+                    id: { type: GraphQLNonNull(GraphQLInt) }
+                },
+                resolve: (root, args) => {
+                    // console.log( '-----args', args)
+                    // console.log( '-----root', root)
+                    return characters.find(character => character.id === args.id)
+                }
+                // films: {
+                //     MovieType,
+                //     args: {
+                //         id: {type: GraphQLNonNull(GraphQLInt)}
+                //     },
+                //     resolve: (root, args) => {
+                //         console.log( '-----args', args)
+                //         console.log( '-----root', root)
+                //     }
+                // }
             }
         }
     }
 })
 
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: () => {
+      return {
+        deletePerson: {
+          type: PersonType,
+          args: { id: { type: GraphQLNonNull(GraphQLInt) } },
+          resolve: (parentVal, args) => {
+            let character = characters.find(e => e.id === args.id)
+            characters = characters.filter(person => person.id !== args.id)
+            return {
+              id: character.id,
+              name: character.name
+            }
+          }
+        }
+      }
+    }
+  })
+  
+  module.exports = new GraphQLSchema({
+    query: Query,
+    mutation: Mutation
+  })
+
 module.exports = new GraphQLSchema({
-    query: Query
+    query: Query,
+    mutation: Mutation
 })
